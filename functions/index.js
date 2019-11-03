46c1a2d357c85b245c23497298589c5b6da4f423
Justin Ndwiga <ndwigajustin@gmail.com>
2019-11-03 20:58:30 +0300

    - implement signup & login

--- functions/index.js
+++ functions/index.js
@@ -61,4 +61,110 @@ app.post("/buzzes", (req, res) => {
     });
 });
 
+const isEmpty = (string) =>{
+  if(string.trim()  === '') return true;
+  else return false;
+};
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else return false; 
+}
+
+//Signup
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  //validate
+  let errors = {};
+  
+  if(isEmpty(newUser.email)){
+    errors.email = 'Must not be empty';
+  } else if(!isEmail(newUser.email)){
+    errors.email = 'Email is invalid';
+  }
+  
+  if(isEmpty(newUser.password)) errors.password = 'Must not be mepty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword ='Passwords don\'t match';
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be mepty';
+  
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) =>{
+      if(doc.exists){
+        return res.status(400).json({handle: 'handle already taken'});
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) =>{
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() =>{
+      return res.status(201).json({token});
+    })
+    .catch(err => {
+      console.error(err);
+      if(err.code === 'auth/email-already-in-use'){
+        return res.status(400).json({email: 'Email is already in use'});
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+app.post('/login', (req, res) =>{
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if(isEmpty(user.email)) errors.email = 'Must not be empty';
+  if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+  .auth()
+  .signInWithEmailAndPassword(user.email, user.password)
+  .then(data => {
+    return data.user.getIdToken();
+  })
+  .then(token =>{
+    return res.json({token});
+  })
+  .catch(err =>{
+    console.error(err)
+    if(err.code === 'auth/wrong-password'){
+      return res.status(403).json({general: 'Wrong credentials, please retry'})
+    } else {
+      return res.status(500).json({error: err.code})
+    }
+  })
+});
+
 exports.api = functions.region("europe-west1").https.onRequest(app);
