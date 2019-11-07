d329a51788801d762b56d8605aa0e3faadf1a989
Justin Ndwiga <ndwigajustin@gmail.com>
2019-11-07 21:31:47 +0300

    - add like notificatins - add comment notifications

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const FBAuth = require("./utils/firebaseAuth");
+const { db } = require("./utils/admin");
 const {
   getAllBuzzes,
   postBuzz,
@@ -11,6 +12,7 @@ const {
   unlikeBuzz,
   deleteBuzz
 } = require("./handlers/buzzes");
+
 const {
   signUp,
   login,
@@ -36,3 +38,66 @@ app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
+
+exports.createNotificationOnLike = functions.region('europe-west1').firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/buzzes/${snapshot.data().buzzId}`).get()
+      .then(doc => {
+        if(doc.exists){
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            buzzId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      })
+  });
+
+  exports.deleteNotificationOnUnlike = functions
+    .region('europe-west1')
+    .firestore.document('likes/{id}')
+    .onDelete((snapshot) => {
+      db.doc(`/notifications/${snapshot.id}`)
+        .delete()
+        .then(() => {
+          return;
+        })
+        .catch(err => {
+          console.error(err);
+          return;
+        })
+  })
+
+exports.createNotificationOnComment = functions.region('europe-west1').firestore.document('comments/{id}')
+.onCreate((snapshot) => {
+  db.doc(`/buzzes/${snapshot.data().buzzId}`).get()
+    .then(doc => {
+      if(doc.exists){
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createdAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'comment',
+          read: false,
+          buzzId: doc.id
+        });
+      }
+    })
+    .then(() => {
+      return;
+    })
+    .catch(err => {
+      console.error(err);
+      return;
+    })
+})
